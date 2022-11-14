192baeb766717f18109513ef842a902cabf20a3a
duncanlang <duncan.lang@me.com>
2022-11-13 20:37:11 -0800

    Init default settings on options page

--- options.js
+++ options.js
@@ -1,22 +1,28 @@
 var options;
 
 // Load from storage
-async function load(){
+async function load() {
     options = await browser.storage.local.get().then(function (storedSettings) {
         return storedSettings;
     });
+    // Init default settings
+    if (options['imdb-enabled'] == null) options['imdb-enabled'] = true;
+    if (options['tomato-enabled'] == null) options['tomato-enabled'] = true;
+    if (options['metacritic-enabled'] == null) options['metacritic-enabled'] = true;
+    if (options['mal-enabled'] == null) options['mal-enabled'] = true;
+    if (options['al-enabled'] == null) options['al-enabled'] = true;
+    if (options['cinema-enabled'] == null) options['cinema-enabled'] = true;
     set();
 }
 
-function save(){ //key, value){
-    //options[key] = value;
+function save() {
     browser.storage.local.set(options);
 }
 
-function set(){
+function set() {
     Object.keys(options).forEach(key => {
         var element = document.querySelector('#' + key);
-        if (element != null){
+        if (element != null) {
             switch (typeof (options[key])) {
                 case ('boolean'):
                     element.checked = options[key];
@@ -25,8 +31,6 @@ function set(){
                     element.value = options[key];
                     break;
             }
-
-            //element.value = options[key];
         }
     })
 }
@@ -43,7 +47,7 @@ document.addEventListener('change', event => {
             break;
     }
 
-    save();//event.target.id, event.target.value);
+    save();
 });
 
 // On load, load
